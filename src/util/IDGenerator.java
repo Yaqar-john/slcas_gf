0d2f5f69193ca3346995fed27bcf21a3a021a1e7
successjoseph <successjoseph67@gmail.com>
2026-03-10 21:01:11 +0100

    implemented IDGenerator method
    Implemented generateId method to format IDs with a prefix and incremented counter.

--- src/util/IDGenerator.java
+++ src/util/IDGenerator.java
@@ -2,5 +2,8 @@ package util;
 
 public class IDGenerator {
 
-    public static String generateId
+    public static String generateId (String prefix, int currentNumber){
+        int nextNumber = currentNumber + 1;
+        return String.format("%s%06d", prefix, nextNumber);
+    }
 }
